9b9e64720fc31d665102c8be7f880e8a18741d48
Ben Dubroff <BLDubroff@gmail.com>
2023-02-18 12:09:52 -0500

    indexjsx

---
+++ views/places/index.jsx
@@ -0,0 +1,15 @@
+const React = require('react')
+const Def = require('./default')
+
+function index () {
+    return (
+      <Def>
+          <main>
+              <h1>Index</h1>
+              <p>Stub html</p>
+          </main>
+      </Def>
+    )
+  }
+
+module.exports = index
