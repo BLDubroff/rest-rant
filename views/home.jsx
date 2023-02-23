3c2399a585c53090d65294a70b334358e6bdc837
Ben Dubroff <BLDubroff@gmail.com>
2023-02-22 19:46:33 -0500

    h1 title update

--- views/home.jsx
+++ views/home.jsx
@@ -5,16 +5,17 @@ function home () {
     return (
       <Def>
           <main>
-              <h1>HOME</h1>
+              <h1>REST-rant</h1>
               <a href="/places">
+              <button className="btn-primary">Places Page</button>
+              </a>
                 <div>
                   <img id='raspberry-cake' src="/images/raspberry-cake.jpg" alt="Raspberry Cake"/>
                   <div>
                   Photo by <a href="https://unsplash.com/@anna_tukhfatullina?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Anna Tukhfatullina Food Photographer/Stylist</a> on <a href="https://unsplash.com/images/food?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
                   </div>
                 </div>
-              <button className="btn-primary">Places Page</button>
-              </a>
+
           </main>
       </Def>
     )
