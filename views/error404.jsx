bcd5010d6e1e2c22123eb1bfb2efbd66ab4ed0d4
Ben Dubroff <BLDubroff@gmail.com>
2023-02-22 19:35:28 -0500

    added images

--- views/error404.jsx
+++ views/error404.jsx
@@ -7,6 +7,12 @@ function error404 () {
           <main>
               <h1>404: PAGE NOT FOUND</h1>
               <p>Oops, sorry, we can't find this page!</p>
+              <div>
+                  <img id='code-404' src="/images/code-404.jpg" alt="404 Error"/>
+                  <div>
+                  Photo by <a href="https://unsplash.com/@markusspiske?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Markus Spiske</a> on <a href="https://unsplash.com/backgrounds/apps/website?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+                  </div>
+                </div>
           </main>
       </Def>
     )
