854619661b972c1406d25021d5e80d9b884e55ef
Ben Dubroff <BLDubroff@gmail.com>
2023-02-27 19:44:41 -0500

    added show pages

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,11 +1,19 @@
 const React = require('react')
 const Def = require('../default')
 
-function show () {
+function show (data) {
     return (
         <Def>
           <main>
-            <h1>Show Page</h1>
+            <h1>{ data.place.name }</h1>
+            <div>
+                <h2>Ratings</h2>
+                <h3>Not yet rated</h3>
+            </div>
+            <div>
+                <h2>Description</h2>
+                <h3>Located in {data.place.city}, {data.place.state} and serving {data.place.cuisines}</h3>
+            </div>
           </main>
         </Def>
     )
