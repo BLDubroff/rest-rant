7fab6eeb2b590823539406edb2466b8a723e2e52
Ben Dubroff <BLDubroff@gmail.com>
2023-02-27 20:36:47 -0500

    Put route still not working

--- controllers/places.js
+++ controllers/places.js
@@ -43,6 +43,30 @@ router.get('/:id', (req, res) => {
   }
 })
 
+// PUT
+router.put('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+      res.render('error404')
+  }
+  else if (!places[id]) {
+      res.render('error404')
+  }
+  else {
+      if (!req.body.pic) {
+          // Default image if one is not provided
+          req.body.pic = 'http://placekitten.com/400/400'
+      }
+      if (!req.body.city) {
+          req.body.city = 'Anytown'
+      }
+      if (!req.body.state) {
+          req.body.state = 'USA'
+      }
+      places[id] = req.body
+      res.redirect(`/places/${id}`)
+  }
+})
 // DELETE
 router.delete('/:id', (req, res) => {
   let id = Number(req.params.id)
@@ -69,20 +93,6 @@ router.get('/:id/edit', (req, res) => {
   }
   else {
     res.render('places/edit', { place: places[id] })
-  }
-})
-
-// PUT
-router.put('/:id', (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
-      res.render('error404')
-  }
-  else if (!places[id]) {
-      res.render('error404')
-  }
-  else {
-      res.redirect(`/places/${id}`)
   }
 })
 
