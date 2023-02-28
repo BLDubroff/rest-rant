3c4dc2dfa6232b60718b1a915dca691b2669f764
Ben Dubroff <BLDubroff@gmail.com>
2023-02-27 19:54:36 -0500

    delete functionality implemented

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -5,15 +5,23 @@ function show (data) {
     return (
         <Def>
           <main>
+                <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
+                    Edit
+                    </a>      
+            <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
+                <button type="submit" className="btn btn-danger">
+                    Delete
+                </button>
+            </form> 
             <h1>{ data.place.name }</h1>
-            <div>
-                <h2>Ratings</h2>
-                <h3>Not yet rated</h3>
-            </div>
-            <div>
-                <h2>Description</h2>
-                <h3>Located in {data.place.city}, {data.place.state} and serving {data.place.cuisines}</h3>
-            </div>
+                <div>
+                    <h2>Ratings</h2>
+                    <h3>Not yet rated</h3>
+                </div>
+                <div>
+                    <h2>Description</h2>
+                    <h3>Located in {data.place.city}, {data.place.state} and serving {data.place.cuisines}</h3>
+                </div>
           </main>
         </Def>
     )
